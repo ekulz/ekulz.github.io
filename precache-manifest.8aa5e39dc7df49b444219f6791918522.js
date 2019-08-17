ab2de7b3bfa9e224b8a25f33b6181f3b2a667021
ekulz <lukehickin@hotmail.com>
2019-08-17 22:07:11 +1200

    Initial commit from Github Pages deploy

---
+++ precache-manifest.8aa5e39dc7df49b444219f6791918522.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9dbd0ed7fdffcfb3172bc71d541e3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a9574d213160862e034",
+    "url": "/static/css/main.7d68695b.chunk.css"
+  },
+  {
+    "revision": "63d5fcb917589d08e171",
+    "url": "/static/js/2.59c43937.chunk.js"
+  },
+  {
+    "revision": "5a9574d213160862e034",
+    "url": "/static/js/main.f6c44cc4.chunk.js"
+  },
+  {
+    "revision": "86c3d9026d9463784443",
+    "url": "/static/js/runtime~main.fac22df3.js"
+  }
+]);
