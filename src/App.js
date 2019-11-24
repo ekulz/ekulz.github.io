8b3a64f8d2296b25bd6bddcb990378e0be6e2060
ekulz <lukehickin@hotmail.com>
2019-11-24 18:56:23 +1300

    Format header, and add more content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import './App.css';
-import { Container, Header, Divider, Icon } from 'semantic-ui-react'
+import { Container, Header, Divider, Icon, Segment, Button } from 'semantic-ui-react'
 
 const App = () => (
   <div className="App">
     <Container text textAlign='center'>
-      <Header as='h1' icon textAlign='center' color='blue'>
-        <Icon name='terminal' circular/>
-        <Header.Content>Luke Hickin</Header.Content>
-      </Header>
+
+      <Segment basic className='segmentHeader' size='huge'>
+        <Header as='h1' icon textAlign='center' color='blue'>
+          <Icon name='terminal' circular/>
+          <Header.Content>Luke Hickin</Header.Content>
+        </Header>
+      </Segment>
+
+      <Divider className='dividerHeader'/>
+
+      <Segment basic size='large' textAlign='center'>
+        <p>
+          Hi, I'm <b>Luke</b>. 
+          <span role="img" aria-label="wave">👋</span>
+        </p>
+        <p>
+          I live in Auckland, New Zealand. 
+          <span role="img" aria-label="flag-nz">🇳🇿</span>
+        </p>
+        <p>
+          I'm a software engineer currently working at <b>Xero</b>. 
+          <span role="img" aria-label="laptop">💻</span>
+        </p>
+        <p>
+          I love doing dev, I love doing ops, but it's even better when I can do both. 
+          <span role="img" aria-label="nerd-face">🤓</span></p>
+        <div>
+          In my spare time, you'll find me hanging out with my friends, gaming, or playing soccer.<br></br>
+          I'm a keen snowboarder in the winter, and to keep fit I've started learning kickboxing.<br></br>
+          I also <b>love</b> cars. Like, a lot. My current daily is a BMW E90 330i. 
+          <span role="img" aria-label="heart-eyes">😍</span>
+        </div>
+      </Segment>
+
       <Divider />
-      <p>Hi, I'm Luke. 👋</p>
-      <p>I live in Auckland, New Zealand. 🇳🇿</p>
-      <p>I'm a software engineer currently working at Xero. 💻</p>
-      <p>I love doing dev, and I love doing ops, but it's even better when I can do both. 🤓</p>
-      <Divider />
-      <p></p>
+
     </Container>
   </div>
 );
