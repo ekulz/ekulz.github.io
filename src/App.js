256cd009bbddf7661b3493f6a4401914b97746ad
ekulz <lukehickin@hotmail.com>
2020-03-22 00:03:22 -0700

    Add GA

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import ReactGA from 'react-ga';
 import MenuBar from './components/MenuBar'
 import Home from './components/Home'
 
 const App = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+
   return (
     <div className="App">
       <MenuBar />
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import 'semantic-ui-css/semantic.min.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize("UA-161575351-1");
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
