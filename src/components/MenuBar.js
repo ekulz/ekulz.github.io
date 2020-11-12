76ee7293f19770e21ea0c9f82c4c14422b62c397
ekulz <lukehickin@hotmail.com>
2020-11-11 16:34:04 -0800

    Removed unused nav link

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,28 +1,22 @@
-import React, { useState }  from 'react'
+import React  from 'react'
 import { Menu } from 'semantic-ui-react'
 import classNames from 'classnames'
 import useDarkMode from 'use-dark-mode';
 import DarkModeToggle from './DarkModeToggle'
 
 const MenuBar = () => {
-    const [activeItem, updateActiveItem] = useState('home');
     const darkMode = useDarkMode(false);
 
     var menuClass = classNames('secondary', 'pointing', { 'inverted': darkMode.value} );
 
     return (
-        <Menu className={menuClass}>
-          <Menu.Item
-            name='home'
-            active={activeItem === 'home'}
-            onClick={() => updateActiveItem('home')}
-          />
-          <Menu.Menu position='right'>
-            <Menu.Item>
-              <DarkModeToggle/>
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu>
+      <Menu className={menuClass}>
+        <Menu.Menu position='right'>
+          <Menu.Item>
+            <DarkModeToggle/>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     );
 };
 
